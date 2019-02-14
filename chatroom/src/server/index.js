2680ae01e10dc2fed3bd300ab245edbc72798a05
ilozanof <lozanofernandez@yahoo.es>
2019-02-14 16:20:50 +0100

    initial commit

---
+++ chatroom/src/server/index.js
@@ -0,0 +1,9 @@
+//var server = require('./httpServer2');
+
+import HttpServer from './httpServer';
+
+console.log("hello World!!");
+console.log("Hi again!!!!");
+// we start the server...
+var server = new HttpServer();
+server.start();
